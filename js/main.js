5305659c1956de43c34dba5fdfd452d9ee6f958d
nukrig <n_gegelashvili@cu.edu.ge>
2023-02-11 00:51:36 +0400

    little update on information html
    on the number input added onclick listener, if you click,the value becomes +995 automatically

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ import { formData } from "./data.js";
 import { fetchGet } from "./fetch.js";
 import { inputExport } from "./inputs.js";inputExport();
 import { pageButtonsExport } from "./buttons.js";pageButtonsExport();
+        
